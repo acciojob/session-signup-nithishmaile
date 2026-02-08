88b706333afbff46447390710c2fdb72324ddfb4
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2026-02-08 15:00:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const form=document.getElementById("myForm");
+
+function onSubmitForm(e){
+	e.preventDefault()
+	const name=e.target[0].value
+	const email=e.target[1].value
+	const password=e.target[2].value
+	const confirmPassword=e.target[3].value
+	if(password===confirmPassword){
+		sessionStorage.setItem("user",JSON.stringify({name,email,password}))
+		return
+	}
+	alert("Passwords do not match")
+}
+
+form.addEventListener("submit",onSubmitForm)
